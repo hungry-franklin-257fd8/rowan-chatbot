9e94f85e7d9ba7130fa725d8bc55415f01ecacba
Vince <45806875+vince1444@users.noreply.github.com>
2022-02-21 06:37:34 -0500

    Added /campus command & started welcome message functionality

--- bot-start.js
+++ bot-start.js
@@ -17,17 +17,30 @@ client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}...`);
 });
 
+// code this when people are online so I can test
+client.on('guildMemberAdd', async (member) => {
+    member.guild.channels.get('channelID')
+        .send(`Welcome, ${member}!`);
+});
+
 client.on('interactionCreate', async (interaction) => {
     const { commandName } = interaction;
 
     if (!interaction.isCommand()) return;
 
-    if (commandName === 'help') {
+    if (commandName === 'help') 
+    {
         await interaction.reply('`Coming soon.`');
     }
 
-    if (commandName === 'info') {
+    if (commandName === 'info') 
+    {
         await interaction.reply('`Coming soon.`');
+    }
+
+    if (commandName === 'campus')
+    {
+        await interaction.reply('https://www.rowan.edu/about/visiting/main.html');
     }
 });
 
