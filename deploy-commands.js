a71844cc97f19272c84e8ba17ecd2de63fed0b2f
admurable <tasbih.babar@gmail.com>
2022-03-23 19:54:28 -0400

    Fixed deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,9 +7,10 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9')
 const fs = require("fs");
 require('dotenv').config();
-const TOKEN = process.env.TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
+const config = require('./config.json')
+const TOKEN = config.TOKEN;
+const CLIENT_ID = config.CLIENT_ID;
+const GUILD_ID = config.GUILD_ID;
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const rest = new REST({ version: '9' }).setToken(TOKEN);
@@ -21,11 +22,11 @@ const rest = new REST({ version: '9' }).setToken(TOKEN);
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+    commands.push(new SlashCommandBuilder().setName(command.name).setDescription(command.description));
 }
 
 (async () => {
-        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
-            .then(() => console.log('Successfully registered application commands.'))
-            .catch(console.error);
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+        .then(() => console.log('Successfully registered application commands.'))
+        .catch(console.error);
 })();
