486218c88a0a1c6d3db2cc94586918974f3461ab
Mura <57267395+admurable@users.noreply.github.com>
2022-05-04 04:50:27 -0400

    Added error checks

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,8 +1,12 @@
 module.exports = async (client, interaction) => {
-    // begone
-    if (!interaction.isCommand()) return;
+    try {
+        if (!interaction.isCommand()) return;
 
-    const cmd = client.commands.get(interaction.commandName);
+        const cmd = client.commands.get(interaction.commandName);
 
-    await interaction.reply(await cmd.run(client, interaction));
+        await interaction.reply(await cmd.run(client, interaction));
+    } catch (e) {
+        await interaction.reply("Something went wrong when running this command. Check the console for more info.");
+        console.error(e);
+    }
 }
