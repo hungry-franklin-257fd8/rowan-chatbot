c855abe36d6dbb9f18912acf327f9eb8fbd5fc5b
Mura <57267395+admurable@users.noreply.github.com>
2022-05-05 17:28:00 -0400

    included Avery's website

--- commands/bot.js
+++ commands/bot.js
@@ -4,7 +4,10 @@ exports.run = async () => {
     const embed = new MessageEmbed()
         .setColor(generateRandomColour())
         .setAuthor({ name: 'Rowan Chatbot', iconURL: 'https://i.imgur.com/rotM2an.png' })
-        .setDescription('This bot was made for Software Engineering during the Spring 2022 semester. It is made by Vince, Mura, Avery, Joe, Tristan, & Zach. The GitHub link can be found here: https://github.com/hungry-franklin-257fd8/rowan-chatbot.')
+        .setDescription('This bot was made for Software Engineering during the Spring 2022 semester. ' +
+            'It is made by Vince, Mura, Avery, Joe, Tristan, & Zach. ' +
+            'The GitHub link can be found [here](https://github.com/hungry-franklin-257fd8/rowan-chatbot).' +
+            '\nWe also have a [website](http://elvis.rowan.edu/~romano23/WebProgramming/SWENG/index.html) hosted on Rowan Elvis!')
         .setTimestamp()
         .setFooter({text: `Made with \u2665`});
     return { embeds: [embed] };
